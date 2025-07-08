bcd37b64c0e135768fb5f14f821dd86044153c03
dongyoon7212 <dongyoon7212>
2025-07-08 20:21:14 +0900

    feat: board

---
+++ src/main/java/com/korit/BoardStudy/mapper/BoardMapper.java
@@ -0,0 +1,9 @@
+package com.korit.BoardStudy.mapper;
+
+import com.korit.BoardStudy.entity.Board;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+    int addBoard(Board board);
+}
